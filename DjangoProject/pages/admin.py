5fe3f2a67187511240a491ef9d835123c2047b9c
Eduardor9 <eduardoromeror9@outlook.com>
2021-03-09 05:22:54 -0300

    Paginas creadas con menu

--- DjangoProject/pages/admin.py
+++ DjangoProject/pages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Page
 
 # Register your models here.
+admin.site.register(Page)
+
+# Configuracion del Panel
+
+title = 'Project ADMIN'
+subtitle = 'Management Panel'
+
+admin.site.site_header = title
+admin.site.site_title = title
+admin.site.index_title = subtitle
