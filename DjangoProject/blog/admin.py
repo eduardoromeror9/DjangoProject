93d0d0049f4c008b0c40d38f64e6c98d6e9c6ed4
Eduardor9 <eduardoromeror9@outlook.com>
2021-03-12 04:03:44 -0300

    Subir imagenes ok

--- DjangoProject/blog/admin.py
+++ DjangoProject/blog/admin.py
@@ -7,7 +7,12 @@ class CategoryAdmin(admin.ModelAdmin):
     
     
 class ArticleAdmin(admin.ModelAdmin):    
-    readonly_fields = ('created_at', 'updated_at')
+    readonly_fields = ('user', 'created_at', 'updated_at')
+    
+    def save_model(self, request, obj, form, change):
+        if not obj.user_id:
+            obj.user_id = request.user.id
+        obj.save()
 
 
 # Register your models here.
