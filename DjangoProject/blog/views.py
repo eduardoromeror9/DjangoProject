5f565e28bc79e74895c544e74445b5378788722f
Eduardor9 <eduardoromeror9@outlook.com>
2021-03-14 04:45:13 -0300

    Registro de usuarios agregado

--- DjangoProject/blog/views.py
+++ DjangoProject/blog/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from blog.models import Category, Article
 
 # Create your views here.
 def list(request):
     
+    # Sacar articulos
     articles = Article.objects.all()
+    
+    # Paginar los articulos
+    paginator = Paginator(articles, 1)
+    
+    
+    # Recoger numero de pagina
+    page = request.GET.get('page')
+    page_articles = paginator.get_page(page) 
+    
     
     return render(request, 'articles/list.html', {
         'title': 'Articulos',
-        'articles': articles
+        'articles': page_articles
     })
     
     
@@ -19,4 +30,13 @@ def category(request, category_id):
     
     return render(request, 'categories/category.html', {
         'category': category,
+    })
+    
+
+def article(request, article_id):
+    
+    article = get_object_or_404(Article, id=article_id)
+    
+    return render(request, 'articles/detail.html', {
+        'article': article,
     })
