b00e82fb98c63d17796b9bc247ab03f5231acbff
Eduardor9 <eduardoromeror9@outlook.com>
2021-03-15 01:25:56 -0300

    Login creado

--- DjangoProject/mainapp/views.py
+++ DjangoProject/mainapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from mainapp.forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -28,6 +30,7 @@ def register_page(request):
         
         if register_form.is_valid():
             register_form.save()
+            messages.success(request, 'Te has registrado correctamente!')
             
             return redirect('inicio')            
             
@@ -35,4 +38,23 @@ def register_page(request):
     return render(request, 'users/register.html', {
         'title': 'Registro',
         'register_form': register_form
+    })
+    
+
+def login_page(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')
+        else:
+            messages.warning(request, 'No te has identificado correctamente!')
+    
+    return render(request, 'users/login.html',{
+        'title': 'Identificate',
     })
